46a3893a1754f799950d254703fcf441d206e14a
erwan.vivien <erwan.vivien@epita.fr>
2022-12-26 11:28:05 +0100

    feat: added small script to run nsight

---
+++ ncu.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# Hack to find ncu
+ncu_store=$(ls -1 /nix/store | grep nsight_compute)
+ncu=/nix/store/${ncu_store}/nsight-compute/2022.1.1/ncu
+
+OUTPUT_PATH=${1}
+BENCH_PATH=${2:-./build/kernel}
+
+if [ -z "${OUTPUT_PATH}" ]; then
+    echo "usage: $0 <output>"
+    exit 1
+fi
+
+if ! [ -d "build" ]; then
+    cmake -B build
+fi
+make -C build -j4
+
+date=$(date +'%F-%Hh%Mm%Ss')
+
+echo "${ncu}" -o "${OUTPUT_PATH}" -f --set full "${BENCH_PATH}" --bench-nsight
+"${ncu}" \
+  -o "${date}_${OUTPUT_PATH}" \
+  --metrics regex:data_bank_conflicts_pipe_lsu_mem_shared_op \
+  -f --set full \
+  "${BENCH_PATH}" --bench-nsight
