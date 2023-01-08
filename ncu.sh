972816a6b78eeafe443a334e62cebfe70d5b7765
erwan.vivien <erwan.vivien@epita.fr>
2023-01-08 22:07:18 +0100

    fix: ncu was not finding right ncu path

--- ncu.sh
+++ ncu.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 # Hack to find ncu
-ncu_store=$(ls -1 /nix/store | grep nsight_compute)
+ncu_store=$(ls -1 '/nix/store' | grep 'nsight_compute' | grep -v 'drv')
 ncu=/nix/store/${ncu_store}/nsight-compute/2022.1.1/ncu
 
 OUTPUT_PATH=${1}
 BENCH_PATH=${2:-./build/kernel}
 
 if [ -z "${OUTPUT_PATH}" ]; then
-    echo "usage: $0 <output>"
+    echo "usage: $0 <output> [binary]"
     exit 1
 fi
 
@@ -22,6 +22,7 @@ date=$(date +'%F-%Hh%Mm%Ss')
 echo "${ncu}" -o "${OUTPUT_PATH}" -f --set full "${BENCH_PATH}" --bench-nsight
 "${ncu}" \
   -o "${date}_${OUTPUT_PATH}" \
-  --metrics regex:data_bank_conflicts_pipe_lsu_mem_shared_op \
   -f --set full \
   "${BENCH_PATH}" --bench-nsight
+
+  # --metrics regex:data_bank_conflicts_pipe_lsu_mem_shared_op \
